c2b7d777a9202fe58a63cd85cd2479ab9dc87fa2
Syamkarni Uppalapati <101443460+syamkarni@users.noreply.github.com>
2024-03-25 02:31:59 +0530

    backend done

--- backend/app.py
+++ backend/app.py
@@ -11,8 +11,6 @@ def scrape_data():
     soup = BeautifulSoup(response.content, 'html.parser')
     data = []
     for row in soup.find_all('tr'):
-      if len(data) >= 20:
-        break
       cells = row.find_all('td')
       if len(cells) > 1:
           stock_info = {}
@@ -33,15 +31,17 @@ def indi_data_link(number):
    for i in d:
       if i['number']==number:
          return i['link']
-      
-def indi_data(name):
-    url=indi_data_link(name)
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-    }
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    data = []
+
+def indi_data_graph(number):
+    d = scrape_data()
+    for i in d:
+        if i['number'] == number:
+            url = i['link']
+            stock_identifier = url.rsplit('/', 1)[-1]
+            api_url = f"https://api.moneycontrol.com/mcapi/v1/stock/estimates/price-forecast?scId={stock_identifier}&ex=N&deviceType=W"
+            return api_url
+    return None
+
 
 def get_individual_stock_data(number):
     stock_url = indi_data_link(number)
@@ -50,16 +50,25 @@ def get_individual_stock_data(number):
     }
     response = requests.get(stock_url, headers=headers)
     soup = BeautifulSoup(response.content, 'html.parser')
+
     stock_data = {}
-    table_rows = soup.select_one('div.oview_table').find_all('tr')
-    for tr in table_rows:
-        cells = tr.find_all('td')
-        if len(cells) == 2:
-            key = cells[0].get_text(strip=True)
-            value = cells[1].get_text(strip=True)
-            stock_data[key] = value
+    name_sector_div = soup.find('div', class_='inid_name')
+    stock_data['Name'] = name_sector_div.find('h1').get_text(strip=True)
+    sector_strong = name_sector_div.find('strong')
+    stock_data['Sector'] = sector_strong.get_text(strip=True)
+    stock_data['Graph_link']= indi_data_graph(number)
+    oview_tables = soup.find_all('div', class_='oview_table')
+    for table in oview_tables[:2]:
+        table_rows = table.find_all('tr')
+        for tr in table_rows:
+            cells = tr.find_all('td')
+            if len(cells) == 2:
+                key = cells[0].get_text(strip=True).replace('i', 'VWAP')
+                value = cells[1].get_text(strip=True)
+                stock_data[key] = value
 
     return stock_data
+
 
 
 
